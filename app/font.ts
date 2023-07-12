95973d3c512e36401991211953c7fbedd6319044
brain <vaibhav.satam@accionlabs.com>
2023-07-12 13:05:32 +0530

    first commit

---
+++ app/font.ts
@@ -0,0 +1,8 @@
+import { Roboto } from 'next/font/google';
+
+export const FontProvider = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: ['Helvetica', 'Arial', 'sans-serif'],
+});
